d7660103e7569875faedd92e2434555b0ff74289
yaevvv <yaevvv@gmail.com>
2020-05-01 08:36:41 +0300

    new tf vpc

---
+++ terra-aws-vpc/variables.tf
@@ -0,0 +1,37 @@
+# variables.tf
+variable "region" {
+     default = "us-east-1"
+}
+variable "availabilityZone" {
+     default = "us-east-1a"
+}
+variable "instanceTenancy" {
+    default = "default"
+}
+variable "dnsSupport" {
+    default = true
+}
+variable "dnsHostNames" {
+    default = true
+}
+variable "vpcCIDRblock" {
+    default = "17.20.0.0/16"
+}
+variable "subnetCIDRblock" {
+    default = "172.20.1.0/24"
+}
+variable "destinationCIDRblock" {
+    default = "0.0.0.0/0"
+}
+variable "ingressCIDRblock" {
+    type = list
+    default = [ "0.0.0.0/0" ]
+}
+variable "egressCIDRblock" {
+    type = list
+    default = [ "0.0.0.0/0" ]
+}
+variable "mapPublicIP" {
+    default = true
+}
+# end of variables.tf
