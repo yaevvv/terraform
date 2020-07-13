6622426240a9d88c9dd5f2c23ce15f645f72953c
yaevvv <yaevvv@gmail.com>
2020-07-13 16:17:27 +0300

    !!

--- terra-aws-vpc/vpc.tf
+++ terra-aws-vpc/vpc.tf
@@ -1,5 +1,3 @@
-# vpc.tf 
-# Create VPC/Subnet/Security Group/Network ACL
 provider "aws" {
   version = "~> 2.0"
   region     = var.region
@@ -13,8 +11,7 @@ resource "aws_vpc" "My_VPC" {
 tags = {
     Name = "My VPC"
 }
-} # end resource
-# create the Subnet
+} 
 resource "aws_subnet" "My_VPC_Subnet" {
   vpc_id                  = aws_vpc.My_VPC.id
   cidr_block              = var.subnetCIDRblock
@@ -23,14 +20,13 @@ resource "aws_subnet" "My_VPC_Subnet" {
 tags = {
    Name = "My VPC Subnet"
 }
-} # end resource
-# Create the Security Group
+}
 resource "aws_security_group" "My_VPC_Security_Group" {
   vpc_id       = aws_vpc.My_VPC.id
   name         = "My VPC Security Group"
   description  = "My VPC Security Group"
   
-  # allow ingress of port 22
+  
   ingress {
     cidr_blocks = var.ingressCIDRblock  
     from_port   = 22
@@ -38,7 +34,7 @@ resource "aws_security_group" "My_VPC_Security_Group" {
     protocol    = "tcp"
   } 
   
-  # allow egress of all ports
+  
   egress {
     from_port   = 0
     to_port     = 0
@@ -49,8 +45,7 @@ tags = {
    Name = "My VPC Security Group"
    Description = "My VPC Security Group"
 }
-} # end resource
-# create VPC Network access control list
+} 
 resource "aws_network_acl" "My_VPC_Security_ACL" {
   vpc_id = aws_vpc.My_VPC.id
   subnet_ids = [ aws_subnet.My_VPC_Subnet.id ]
@@ -64,7 +59,7 @@ resource "aws_network_acl" "My_VPC_Security_ACL" {
     to_port    = 22
   }
   
-  # allow ingress port 80 
+  
   ingress {
     protocol   = "tcp"
     rule_no    = 200
@@ -74,7 +69,7 @@ resource "aws_network_acl" "My_VPC_Security_ACL" {
     to_port    = 80
   }
   
-  # allow ingress ephemeral ports 
+  
   ingress {
     protocol   = "tcp"
     rule_no    = 300
@@ -84,7 +79,7 @@ resource "aws_network_acl" "My_VPC_Security_ACL" {
     to_port    = 65535
   }
   
-  # allow egress port 22 
+  
   egress {
     protocol   = "tcp"
     rule_no    = 100
@@ -94,7 +89,7 @@ resource "aws_network_acl" "My_VPC_Security_ACL" {
     to_port    = 22
   }
   
-  # allow egress port 80 
+  
   egress {
     protocol   = "tcp"
     rule_no    = 200
@@ -104,7 +99,7 @@ resource "aws_network_acl" "My_VPC_Security_ACL" {
     to_port    = 80 
   }
  
-  # allow egress ephemeral ports
+  
   egress {
     protocol   = "tcp"
     rule_no    = 300
@@ -116,30 +111,25 @@ resource "aws_network_acl" "My_VPC_Security_ACL" {
 tags = {
     Name = "My VPC ACL"
 }
-} # end resource
-# Create the Internet Gateway
+} 
 resource "aws_internet_gateway" "My_VPC_GW" {
  vpc_id = aws_vpc.My_VPC.id
  tags = {
         Name = "My VPC Internet Gateway"
 }
-} # end resource
-# Create the Route Table
+} 
 resource "aws_route_table" "My_VPC_route_table" {
  vpc_id = aws_vpc.My_VPC.id
  tags = {
         Name = "My VPC Route Table"
 }
-} # end resource
-# Create the Internet Access
+} 
 resource "aws_route" "My_VPC_internet_access" {
   route_table_id         = aws_route_table.My_VPC_route_table.id
   destination_cidr_block = var.destinationCIDRblock
   gateway_id             = aws_internet_gateway.My_VPC_GW.id
-} # end resource
-# Associate the Route Table with the Subnet
+} 
 resource "aws_route_table_association" "My_VPC_association" {
   subnet_id      = aws_subnet.My_VPC_Subnet.id
   route_table_id = aws_route_table.My_VPC_route_table.id
-} # end resource
-# end vpc.tf
+} 
